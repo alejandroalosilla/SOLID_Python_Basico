9b74846f55d71971a63a2b38bf78b2d24fe43e21
Alejandro Alosilla <alejandroalosilla@gmail.com>
2023-06-08 23:56:06 -0300

    Create Ex1_Aula2.py

---
+++ Ex1_Aula2.py
@@ -0,0 +1,20 @@
+"""
+(Aula 2)
+Exercicio 1: Faça um formulário que pergunte o nome, cpf, endereço,
+idade, altura e telefone e imprima isso em um relatório organizado
+"""
+
+nome = input('Digite o nome: ').strip().capitalize()
+cpf = input('Digite o CPF: ').strip()
+endereco = input('Digite o endereço: ').strip()
+idade = input('Digite a idade: ').strip()
+altura = input('Digite sua altura: ').strip()
+telefone = input('Digite seu telefone: ').strip()
+
+formulario = {'Nome': nome, 'CPF': cpf, 'Endereço': endereco, 'Idade': idade, 'Altura': altura, 'Telefone': telefone}
+
+print('\nFormulário:')
+print(20 * '-')
+for indice, dado in formulario.items():
+    print(f'{indice}: {dado}')
+print(20 * '-')
